f71ca95bff1e6e17e9446812d74a95e4e7be3827
Danh Nguyen <danh.nguyentrancong>
2023-02-27 16:20:13 +0700

    Change title Next APP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import styles from './page.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+ 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -43,6 +44,7 @@ export default function Home() {
         <div className={styles.thirteen}>
           <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
         </div>
+        
       </div>
 
       <div className={styles.grid}>
